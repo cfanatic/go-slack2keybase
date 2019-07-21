2a46a80c6bbe958a3f49f2af0e14971bb42bff89
Arnd <git@codefanatic.de>
2019-07-21 22:43:18 +0200

    Run Stop() method by defer call

--- main.go
+++ main.go
@@ -14,12 +14,12 @@ const oauth_bot = "<INSERT_BOT_TOKEN>"
 func main() {
 	bridge := bridge.New(oauth_user, oauth_bot, true)
 	bridge.Start()
+	defer bridge.Stop()
 
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	for {
 		<-c
-		bridge.Stop()
 		break
 	}
 }
